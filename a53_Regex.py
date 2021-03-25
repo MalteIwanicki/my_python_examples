66ae3e0d9c9eff4630d4a9c1287020f5d2f12c34
Malte Iwanicki <malteiwa@gmail.com>
2021-03-25 23:26:35 +0100

    [+] regex grouping, escape, pipe |

--- a53_Regex.py
+++ a53_Regex.py
@@ -10,6 +10,7 @@ def is_date(text: str):
                 found = True
                 break
         return found
+
     if len(text) < 10:
         return False
     if not ((text[0:2].isdecimal())
@@ -40,13 +41,14 @@ print(has_date('this is not a date'))  # >>> False
 
 # Or use Regex
 import re
+
 # a date
 # regex strings often have backslashes so we use a raw string
 # r'' to avoid using backslash as an exit char.
 date_re = re.compile(r"\d\d[./-]\d\d[./-]\d\d\d\d")
 mo = date_re.search(message)  # returns a match object
-print(mo) # >>> <re.Match object; span=(36, 46), match='11.06.2021'>
-print(mo.group()) # >>> 11.06.2021
+print(mo)  # >>> <re.Match object; span=(36, 46), match='11.06.2021'>
+print(mo.group())  # >>> 11.06.2021
 print(date_re.findall(message))
 # >>> ['11.06.2021']
 
@@ -54,4 +56,33 @@ print(date_re.findall(message))
 email_re = re.compile(r'[\w-]+@([\w-]+\.)+[\w-]+')
 print(email_re.search(message).group())
 # >>> malteiwa@gmail.com
+
+# Lets say we want the day, month and year of a date seperat:
+# use brackets () to group your data:
+date_splitted = re.compile(r"(\d\d)[./-](\d\d)[./-](\d\d\d\d)")
+
+mo = date_splitted.search(message)  # returns a match object
+
+print(mo.group())  # >>> 11.06.2021
+# .group() returns still the same BUT we can give the group an Index now:
+print('Day: %s  Month: %s  Year: %s' % (mo.group(1), mo.group(2), mo.group(3)))
+# >>> Day: 11  Month: 06  Year: 2021
+print(date_splitted.findall(message))
+# >>> [('11', '06', '2021')]
+
+# () in the regex string group the data, If we are actually looking for ()
+# we need to use escape characters. \( <-would look for literally a bracket.
+regex_with_brackets = re.compile(r"\(\d\d\)[./-]\(\d\d\)[./-]\(\d\d\d\d\)")
+print(regex_with_brackets.findall('11.06.1993')) # cant find a match.
+print(regex_with_brackets.findall('(11).(06).(1993)'))
+# >>> ['(11).(06).(1993)']
+
+# use the | pipesymbol to specify a or:
+regex_or = re.compile(r'(super|spider|ant|bat|iron)man')
+print(regex_or.search('there once was a superhero called antman.').group())
+# >>> antman
+print(regex_or.search('but superman was much stronger.').group())
+# >>> superman
+print(regex_or.findall('superman and batman are friends.'))
+# >>> ['super', 'bat']
 
